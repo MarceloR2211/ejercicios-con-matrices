02f2cf7c0b845047750456e4efb6b36cdc030da9
MarceloR2211 <marcelo6096631@gmail.com>
2024-12-04 17:49:32 -0500

    Update Ejercicio_6_Verificar_simetría_de_una_matriz.java

--- Ejercicio_6_Verificar_simetría_de_una_matriz/src/ejercicio_6_verificar_simetría_de_una_matriz/Ejercicio_6_Verificar_simetría_de_una_matriz.java
+++ Ejercicio_6_Verificar_simetría_de_una_matriz/src/ejercicio_6_verificar_simetría_de_una_matriz/Ejercicio_6_Verificar_simetría_de_una_matriz.java
@@ -31,19 +31,49 @@ public class Ejercicio_6_Verificar_simetría_de_una_matriz {
             System.out.println("");
         }
         
-        int recorrido[] = new int[(f*c)];
-        int axi = 0;
-        
-        for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[0].length; j++) {
-                recorrido[axi] = m[i][j];
-                axi++;
+         //esto crea la matris
+        int arriba = 0;
+        int abajo = f - 1;
+        int izquierda = 0;
+        int derecha = c - 1;
+        //esto son las lineas
+        int a = 0;
+        int recorrido[] = new int[f*c];
+    while (arriba <= abajo && izquierda <= derecha) {
+           
+            for (int i = izquierda; i <= derecha; i++) {
+            recorrido[a] = m[arriba][i]; 
+            a++;    
             }
-            for (int k = 1; k < m[0].length; k++) {
-                recorrido[axi] = m[k][i];
-                axi++;
+            arriba++;
+
+     
+            for (int i = arriba; i <= abajo; i++) {
+            recorrido[a] = m[i][derecha];    
+            a++;       
             }
-        }
+            derecha--; 
+
+ 
+            if (arriba <= abajo) {
+            
+            for (int i = derecha; i >= izquierda; i--) {
+            recorrido[a] = m[abajo][i];  
+            a++;      
+            }
+            abajo--; 
+            }
+
+            if (izquierda <= derecha) {
+              
+            for (int i = abajo; i >= arriba; i--) {
+            recorrido[a] = m[i][izquierda];      
+            a++;      
+            }
+            izquierda++;   
+    }   
+    
+    }
         
         for (int i = 0; i < recorrido.length; i++) {
             System.out.println(recorrido[i]+", ");
